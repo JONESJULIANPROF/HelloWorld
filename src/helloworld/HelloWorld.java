bb55f315fd05eaeb06412319a19c61943ac62688
cannonballjones99@gmail.com <>
2020-02-04 11:52:59 -0500

    this is the first commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author canno
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Jones();
+        }
+    public static void Jones(){
+        System.out.println("Julian Jones!");
+    }
+    
+    }
+
+
